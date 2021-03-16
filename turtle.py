f515cdc2a8d3de2295403d688b82ef11fdd9b521
hanif <mdhanifshaikh007@gmail.com>
2021-03-16 16:14:00 +0530

    pushing file

---
+++ turtle.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.color("red")
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
